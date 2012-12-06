c4bf3dfbe838c1303658b6962af7ca275e4dd9bd
JiankaiDang <jiankaidang@gmail.com>
2012-12-06 00:37:15 -0500

    login with SignPass

--- BackEnd/bank/banksite/urls.py
+++ BackEnd/bank/banksite/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     url(r'^login', 'banksite.views.login', name='login'),
     url(r'^accounts', 'banksite.views.accounts', name='accounts'),
     url(r'^logout', 'banksite.views.logout', name='logout'),
+    url(r'^signpass_login', 'banksite.views.signpass_login'),
 )
