f8f6bf5ef45dac3b45da410f2d32c90ca62751d2
Changpeng <lee_leopold@hotmail.com>
2012-12-06 00:27:11 -0500

    basic communication with ios and bankserver is done

--- BackEnd/SignPass/sign/models.py
+++ BackEnd/SignPass/sign/models.py
@@ -2,34 +2,34 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-  uid=models.IntegerField(primary_key=True)
+#  uid=models.IntegerField(primary_key=True)
   username=models.CharField(max_length=50,unique=True)
   email=models.EmailField(max_length=255,unique=True)  
   def __unicode__(self):
     return self.username
   
 class Piece(models.Model):
-  piece_id=models.IntegerField(primary_key=True)
+#  piece_id=models.IntegerField(primary_key=True)
   signature=models.TextField(null=True)
-  user=models.ForeignKey('User',)
+  user=models.ForeignKey('User')
   def __unicode__(self):
     return self.piece_id
   
 class Service(models.Model):
-  service_id=models.IntegerField(primary_key=True)
+#  service_id=models.IntegerField(primary_key=True)
   service_name=models.CharField(max_length=50,unique=True)
   register_time=models.DateTimeField()
   auth_code=models.CharField(max_length=50,unique=True)
   
 class Binding(models.Model):
-  binding_id=models.IntegerField(primary_key=True)
+#  binding_id=models.IntegerField(primary_key=True)
   service_uid=models.IntegerField()
   binding_time=models.DateTimeField()
   piece=models.ForeignKey('Piece')
   service=models.ForeignKey('Service')
   
 class Verify(models.Model):
-  verify_id=models.IntegerField(primary_key=True)
+#  verify_id=models.IntegerField(primary_key=True)
   binding=models.ForeignKey('Binding')
   timestamp=models.DateTimeField()
   attempt_time=models.SmallIntegerField(default=0)
--- BackEnd/SignPass/sign/views.py
+++ BackEnd/SignPass/sign/views.py
@@ -3,41 +3,61 @@ from models import *
 import datetime
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+import json,sys
 
 def checkUsername(request,username):
   try:
     User.objects.get(username=username)
-    return 0,HttpResponse("s% is existing"% username)
+    return HttpResponse(json.dumps({'success':0,'msg':"username existing"}))
+#    return HttpResponse("Yes")
   except User.DoesNotExist:
-    return 1,HttpResponse("s% is not existing"% username)
+    return HttpResponse(json.dumps({'success':1,'msg':"username not existing"}))
 
-def register(request,username):  
-  if checkUsername(request,username)[0]:
-    return HttpResponse("s% is existing"% username)
-  email=request.POST["email"] 
-  signature=request.POST["signature"]   
-  user=User(username=username,email=email)
-  piece=Piece(user=user,signature=signature)
-  user.save()
-  piece.save()
-  return HttpResponse("%s is created successfully"% username)
+def register(request):   
+  if request.method == 'GET':
+    try:
+      username=request.GET.get('username')
+      print username
+      User.objects.get(username=username)
+      return HttpResponse(json.dumps({'success':0,'msg':"username existing"}))
+    except User.DoesNotExist:
+      email=request.GET.get('email') 
+      print email
+      signature=request.GET.get('signature')  
+      print signature 
+      try:
+        user=User.objects.create(username=username,email=email)
+        Piece.objects.create(user=user,signature=signature)
+      except Exception as e:
+        print '%s (%s)' % (e.message, type(e))
+#      except:
+#        print sys.exc_info
+      return HttpResponse(json.dumps({'success':1,'msg':"%s is created successfully"% username}))
   
-def bind(request,username):
-  try:    
+def bindRequestFromBank(request,service_name,service_uid):
+  try:
+#    service=Service.objects.get(request.POST["service_id"])
+#    service_uid=request.POST["service_uid"]
+    binding=Binding(service_name=service_name,service_uid=service_uid)
+    binding.save()
+    return render_to_response("bind.html",{"service_name":service_name,"service_uid":service_uid}, context_instance=RequestContext(request))    
+  except User.DoesNotExist:
+    return HttpResponse(json.dumps({'success':0,'msg':"username not existing"}))
+  except Piece.DoesNotExist:
+    return HttpResponse(json.dumps({'success':0,'msg':"Piece not existing"}))
+  except Service.DoesNotExist:
+    return HttpResponse(json.dumps({'success':0,'msg':"service %s is not registered"%service_name}))
+  
+def bind(request):
+  if request.method=="POST":
+    service_name=request.POST["service_name"]
+    service_uid=request.POST["service_uid"]
+    username=request.POST["username"]
+    timestamp=datetime.datetime.now()
     user=User.objects.get(username=username)
     piece=Piece.objects.get(user=user)
-    service=Service.objects.get(request.POST["service_id"])
-    service_uid=request.POST["service_uid"]
-    timestamp=datetime.datetime.now()
-    binding=Binding(piece_id=piece,service_id=service,service_uid=service_uid,timestamp=timestamp)
-    binding.save()
-    
-  except User.DoesNotExist:
-    return HttpResponse("s% is not existing"% username)
-  except Piece.DoesNotExist:
-    return HttpResponse("piece_id is not created")
-  except Service.DoesNotExist:
-    return HttpResponse("service is not registered")
+    service=Service.objects.get(service_name=service_name)
+    Binding.objects.create(piece=piece,service=service,service_uid=service_uid,binding_time=timestamp)
 
 def checkServicename(request,service_name):
   try:
@@ -52,11 +72,19 @@ def find_iOS(binding_id):
 def createVerify(binding):
   Verify.objects.create(binding=binding)
   
-def verifySign(binding_id,signature):
+def verifySuccessed(binding_id):
+  return HttpResponse(json.dumps({'success':1,'msg':"message"}))
+
+def checkBinding(request):
+  if request.method == 'POST':
+    service_id=request.POST["service_id"]
+    service_uid=request.POST["service_uid"]
+    return HttpResponse(json.dumps({'output_1':service_id,'output_2':service_uid}))
+  
+def verifyService(binding_id,signature):
   binding=Binding.objects.get(binding_id=binding_id)
   piece=binding.piece
-  original_sign=piece.signature
-  
+  original_sign=piece.signature  
   if signature==original_sign:
     return verifySuccessed(binding_id)
   elif binding.attempt_times<5:
@@ -66,19 +94,42 @@ def verifySign(binding_id,signature):
   else:
     return HttpResponse("attempts exceed 5 times")
   
-def verifySuccessed(binding_id):
-  import json
-  return HttpResponse(json.dumps({'success':1,'msg':"message"}))
-
-def checkBinding(request):
-  if request.method == 'POST':
-    service_id=request.POST["service_id"]
-    service_uid=request.POST["service_uid"]
-    import json
-    return HttpResponse(json.dumps({'output_1':service_id,'output_2':service_uid}))
+def verifySign(request):
+  if request.method == 'GET':
+    try:
+      username=request.GET.get('username')  
+      user=User.objects.get(username=username)     
+      piece=Piece.objects.get(user=user)
+      signature=request.GET.get('signature')
+      if str(signature)==str(piece.signature): 
+        return HttpResponse(json.dumps({'success':1,'msg':"signature matched"}))
+      else:
+        return HttpResponse(json.dumps({'success':0,'msg':"signature not matched"}))
+    except User.DoesNotExist:
+      return HttpResponse(json.dumps({'success':0,'msg':"username not existing"}))
+    except Exception as e:
+      print '%s (%s)' % (e.message, type(e))
+      
+def modifySign(request):
+  if request.method == 'GET':
+    try:
+      username=request.GET.get('username')  
+      user=User.objects.get(username=username)     
+      piece=Piece.objects.get(user=user)
+      if request.GET.get('signature')==None:
+        return HttpResponse(json.dumps({'success':0,'msg':"no signature is caught"}))
+      else: 
+        piece.signature=request.GET.get('signature')
+        return HttpResponse(json.dumps({'success':1,'msg':"signature is modified"}))
+    except User.DoesNotExist:
+      return HttpResponse(json.dumps({'success':0,'msg':"username not existing"}))
+    except Exception as e:
+      print '%s (%s)' % (e.message, type(e))
 
 def index(request): 
   return render_to_response("index.html",{"content":"login content"}, context_instance=RequestContext(request))
+
+
   
 #def reg_service(request,service_name):
 #  try:    
